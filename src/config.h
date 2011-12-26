4ace69f2d3a3cb4bb38b5693e447682fedbf30fe
Sergey Dryabzhinsky <sergey.dryabzhinsky.gmail.com>
2011-12-26 20:42:12 +0000

    Fix for windows build

--- src/config.h
+++ src/config.h
@@ -5,12 +5,31 @@
 #ifndef __CONFIG_H
 #define __CONFIG_H
 
+#ifdef Q_WS_WIN
+
 #ifndef RESOURCES
+    define RESOURCES "."
+#else
+    #undef RESOURCES
     define RESOURCES "."
 #endif
 
 #ifndef ICON
     define ICON "qt-webkit-kiosk.png"
+#else
+    #undef ICON
+    define ICON "qt-webkit-kiosk.png"
+#endif
+
+#else
+
+#ifndef RESOURCES
+    define RESOURCES "."
+#endif
+#ifndef ICON
+    define ICON "qt-webkit-kiosk.png"
+#endif
+
 #endif
 
 #endif
