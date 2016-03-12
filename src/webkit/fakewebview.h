b04a6cb9cdf3d39f9c46e47c5dffe866e3f9d639
Sergey Dryabzhinsky <sergey.dryabzhinsky@gmail.com>
2016-03-12 22:33:04 +0300

    Prepare for webkit/webengine divide

---
+++ src/webkit/fakewebview.h
@@ -0,0 +1,25 @@
+#ifndef FAKEWEBVIEW_H
+#define FAKEWEBVIEW_H
+
+#include <QtNetwork>
+
+#ifdef QT5
+#include <QtWebKitWidgets/QWebView>
+#else
+#include <QWebView>
+#endif
+
+class FakeWebView : public QWebView
+{
+    Q_OBJECT
+
+public:
+    explicit FakeWebView(QWidget* parent = 0);
+
+    void load(const QUrl& url);
+    void load(const QNetworkRequest& request, QNetworkAccessManager::Operation operation = QNetworkAccessManager::GetOperation, const QByteArray &body = QByteArray());
+    void setUrl(const QUrl &url);
+
+};
+
+#endif // FAKEWEBVIEW_H
