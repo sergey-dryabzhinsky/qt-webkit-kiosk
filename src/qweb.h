c8af76f5218115b6bda8985d12ac56e3f7d48036
Sergey Dryabzhinsky <sergey.dryabzhinsky@gmail.com>
2016-03-12 23:16:02 +0300

    Prepare 2

---
+++ src/qweb.h
@@ -0,0 +1,25 @@
+#ifndef QWEB_H
+#define QWEB_H
+
+#ifdef WEB_ENGINE
+
+#include <QtWebEngine>
+#include <QtWebEngineWidgents>
+
+#include "webengine/fakewebview.h"
+#include "webengine/webview.h"
+#endif
+
+#ifdef WEB_KIT
+
+#include <QtWebKit>
+
+#ifdef QT5
+#include <QtWebKitWidgets/QWebInspector>
+#endif
+
+#include "webkit/fakewebview.h"
+#include "webkit/webview.h"
+#endif
+
+#endif // QWEB_H
